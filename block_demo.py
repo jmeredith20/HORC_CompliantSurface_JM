7c1c8eaf48051d4423e9b0072504e9a943c0e556
Jacob Meredith <jmer@udel.edu>
2022-11-26 16:50:01 -0500

    test four

---
+++ block_demo.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from mujoco_py import load_model_from_xml, load_model_from_path, MjSim, MjViewer
+import math
+import os
+
+model = load_model_from_path("./block_demo.xml")
+sim = MjSim(model)
+viewer = MjViewer(sim)
+t = 0
+while True:
+    sim.data.ctrl[0] = math.cos(t / 10.) * 0.01
+    sim.data.ctrl[1] = math.sin(t / 10.) * 0.01
+    t += 1
+    sim.step()
+    viewer.render()
+    if t > 100 and os.getenv('TESTING') is not None:
+        break
