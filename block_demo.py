fd62806fcc3180e9080f78d4a74510a9396bad36
Jacob Meredith <jmer@udel.edu>
2023-01-03 17:11:44 -0500

    Organization and reloading of basic xml file without robot

--- block_demo.py
+++ block_demo.py
@@ -6,12 +6,14 @@ import os
 model = load_model_from_path("./block_demo.xml")
 sim = MjSim(model)
 viewer = MjViewer(sim)
-t = 0
 while True:
-    sim.data.ctrl[0] = math.cos(t / 10.) * 0.01
-    sim.data.ctrl[1] = math.sin(t / 10.) * 0.01
-    t += 1
-    sim.step()
-    viewer.render()
-    if t > 100 and os.getenv('TESTING') is not None:
-        break
+	viewer.render()
+#t = 0
+#while True:
+#    sim.data.ctrl[0] = math.cos(t / 10.) * 0.01
+#    sim.data.ctrl[1] = math.sin(t / 10.) * 0.01
+#    t += 1
+#    sim.step()
+#    viewer.render()
+#    if t > 100 and os.getenv('TESTING') is not None:
+#        break
