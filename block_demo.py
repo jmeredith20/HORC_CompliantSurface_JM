5f1e98afe5cc20d1d1fb24ec337df882d839abd0
Jacob Meredith <jmer@udel.edu>
2023-01-05 15:55:54 -0500

    Made surface bouncy

--- block_demo.py
+++ block_demo.py
@@ -6,20 +6,13 @@ import os
 model = load_model_from_path("./block_demo.xml")
 sim = MjSim(model)
 viewer = MjViewer(sim)
+a = []
 i = 0
 while True:
 	viewer.render()
 	sim.step()
-	if(i == 500):
+	if(i == 2000):
+		print(a)
 		sim.reset();
 		i = 0
 	i += 1
-#t = 0
-#while True:
-#    sim.data.ctrl[0] = math.cos(t / 10.) * 0.01
-#    sim.data.ctrl[1] = math.sin(t / 10.) * 0.01
-#    t += 1
-#    sim.step()
-#    viewer.render()
-#    if t > 100 and os.getenv('TESTING') is not None:
-#        break
